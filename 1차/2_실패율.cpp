6fb12cd7649161c31ebf4e1b4b66facd523606f8
Dongkyu Yu <eastern7star@gmail.com>
2019-03-12 16:20:49 +0900

    Update 2_실패율.cpp

--- 1차/2_실패율.cpp
+++ 1차/2_실패율.cpp
@@ -1,54 +1,32 @@
 #include <iostream>
-#include <string>
 #include <vector>
 #include <map>
-#include <deque>
 #include <algorithm>
 #include <functional>
-#include <iomanip>
-#include <set>
-#include <list>
 
 using namespace std;
 
-// 2019 1st 5hour
+int main() {
+	int N = 5;
+	vector<int> stages = { 2, 1, 2, 6, 2, 4, 3, 3 };
+	vector<int> merge(N + 2);
+	int total = stages.size();
+	for (int s : stages)
+		merge[s]++;
 
-// 2. ½ÇÆÐÀ²
-void falseRate() {
-	vector<int> answer;
-	int N = 4;
-	vector<int> stages = {4, 4, 4, 4, 4};
-	vector<int> stagePass(N + 1);
+	vector<double> failure(1);
 
-	sort(stages.begin(), stages.end(), greater<int>());
-
-	int pass = 0;
-	for (int i = N, j = 0; i >= 0; i--) {
-		for (; j < stages.size(); j++) {
-			if (stages[j] > i)
-				pass++;
-			else
-				break;
-		}
-		stagePass[i] = pass;
-	}
-
-	multimap<float, int, greater<float>> sortedStages;
-	for (int i = 1; i <= N; i++) {
-		if (stagePass[i - 1] == 0)
-			sortedStages.insert(pair<float, int>(0, i));
-		else
-			sortedStages.insert(pair<float, int>(float(stagePass[i - 1] - stagePass[i])/float(stagePass[i - 1]), i));
+	for (int i = 1; i < N + 1; i++) {
+		failure.push_back((double)merge[i] / (double)total);
+		total -= merge[i];
 	}
 	
-	for (multimap<float, int>::iterator s = sortedStages.begin(); s != sortedStages.end(); s++) {
-		answer.push_back(s->second);
-		cout << s->first << ", " << s->second << endl;
-	}
-}
+	multimap<double, int, greater<double>> sorted;
+	for (int i = 1; i < failure.size(); i++)
+		sorted.insert({ failure[i], i });
 
-int main() {
-	falseRate();
+	for (auto s : sorted)
+		cout << s.second << endl;
 
 	return 0;
 }
